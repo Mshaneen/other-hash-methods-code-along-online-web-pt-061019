79b1d1b8f58ff01b74d5a4d68c160d5f9a319880
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-07-13 04:40:15 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -7,13 +7,5 @@
  }
 
 def get_the_values(groceries)
-   groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
- }
-
   groceries.values
-  
 end
